8fd0f19e8c4ccc86098e94027dd29215e9c44600
RotachRoman <cuck.5@mail.ru>
2020-06-28 13:36:32 +0300

    Initial Commit

---
+++ Rate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rate
+//
+//  Created by Rotach Roman on 28.06.2020.
+//  Copyright © 2020 Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
